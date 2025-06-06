75123c8105f8c9cd1bc75cdad25cfba7bf29c987
angelcriber99 <angelcriber99@gmail.com>
2025-06-06 22:42:02 +0200

    Initial Commit

---
+++ GymFit/GymFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymFitApp.swift
+//  GymFit
+//
+//  Created by angel on 6/6/25.
+//
+
+import SwiftUI
+
+@main
+struct GymFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
